d6f7fa6c8c71b3909efbf6f6e0c7afc9f4ab6088
lauropjdutra <lauropjdutra@gmail.com>
2024-04-18 21:25:03 -0300

    put route updated

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express")
 const uuid = require("uuid")
+const cors = require("cors")
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 
 const orders = []
 
 const checkId = (req, res, next) => {
   const { id } = req.params
+  // console.log(id)
   const index = orders.findIndex(p => p.id === id)
   req.orderIndex = index
   req.orderId = id
@@ -33,36 +36,31 @@ app.get("/orders", showMethod, (req, res) => {
   return res.json(orders)
 });
 
-app.post("/orders/new", showMethod, (req, res) => {
-  const { order, clientName, price, status } = req.body
-  const newOrder = { id: uuid.v4(), order, clientName, price, status }
+app.post("/orders", showMethod, (req, res) => {
+  const { order, clientName, price } = req.body
+  const newOrder = { id: uuid.v4(), order, clientName, price }
   orders.push(newOrder)
   return res.status(201).json(newOrder)
 })
 
-app.put("/orders/update/:id", checkId, showMethod, (req, res) => {
+app.put("/orders/:id", checkId, showMethod, (req, res) => {
   const index = req.orderIndex
   const id = req.orderId
-  const { order, clientName, price, status } = req.body
-  const newOrder = { id, order, clientName, price, status }
+  const { order, clientName, price } = req.body
+  const newOrder = { id, order, clientName, price }
   orders[index] = newOrder
   return res.json(orders)
 });
 
-app.delete("/orders/cancel/:id", checkId, showMethod, (req, res) => {
+app.delete("/orders/:id", checkId, showMethod, (req, res) => {
   const index =  req.orderIndex
+  console.log(index)
   orders.splice(index, 1)
   return res.json(orders)
 });
 
 app.get("/orders/filter/:id", checkId, showMethod, (req, res) => {
   const index =  req.orderIndex
-  return res.json(orders[index])
-})
-
-app.patch("/orders/status/:id", checkId, showMethod, (req, res) => {
-  const index = req.orderIndex
-  orders[index].status = "Pronto"
   return res.json(orders[index])
 })
 
